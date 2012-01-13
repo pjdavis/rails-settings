6b327fdb14d17a2771ffea45c9dc70f5c79d671e
PJ Davis <pj.davis@gmail.com>
2012-01-13 13:38:01 -0600

    Forgot to check in generator.

---
+++ lib/generators/rails_settings/rails_settings_generator.rb
@@ -0,0 +1,18 @@
+require 'rails/generators'
+require 'rails/generators/migration'
+require 'rails/generators/active_record/migration'
+
+class RailsSettingsGenerator < Rails::Generators::Base
+
+  include Rails::Generators::Migration
+  extend ActiveRecord::Generators::Migration
+  
+  source_root File.expand_path('../templates', __FILE__)
+  
+  desc 'Generates (but does not run) a migration to add a rails settings table.'
+  
+  def create_migration_file
+    migration_template 'create_settings.rb', 'db/migrate/create_settings.rb'
+  end
+  
+end
